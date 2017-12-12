1f01286fdafb924bd66e2cbd20698b1180adf831
Zhuofan Zhang <zhzhang@imac-025271.colby.edu>
2017-12-12 13:43:41 -0500

    Extracted ocations from the init() method to make it look better

---
+++ GoogleMapsVariables.js
@@ -0,0 +1,100 @@
+
+var mainStString = '<div>173 Main St. & Appleton St.</div>';
+var gilmanStString = '<div>Gilman St.</div>';
+var diamondString = '<div>Diamond building</div>';
+var davisString = '<div>Davis Science Building</div>';
+
+var mainStLatLng = {lat: 44.550999, lng: -69.632022};
+var gilmanStLatLng = {lat: 44.553614, lng: -69.637182};
+var diamondLatLng = {lat: 44.562241, lng: -69.659671};
+var davisLatLng = {lat: 44.564428, lng: -69.658850};
+var mgLatLng_001 = {lat: 44.551679, lng: -69.635225};
+var mgLatLng_002 = {lat: 44.553167, lng: -69.634346};
+
+var gdLatLng_001 = {lat: 44.556345, lng: -69.654988};
+var gdLatLng_002 = {lat: 44.557197, lng: -69.657624};
+var gdLatLng_003 = {lat: 44.557434, lng: -69.658141};
+var gdLatLng_004 = {lat: 44.557957, lng: -69.658622};
+var gdLatLng_005 = {lat: 44.561336, lng: -69.660115};
+var gdLatLng_006 = {lat: 44.561908, lng: -69.660352};
+var gdLatLng_007 = {lat: 44.561983, lng: -69.659704};
+var gdLatLng_008 = {lat: 44.562064, lng: -69.659634};
+
+var ddLatLng_001 = {lat: 44.562267, lng: -69.659589};
+var ddLatLng_002 = {lat: 44.562118, lng: -69.659593};
+var ddLatLng_003 = {lat: 44.561975, lng: -69.659549};
+var ddLatLng_004 = {lat: 44.561865, lng: -69.660306};
+var ddLatLng_005 = {lat: 44.562531, lng: -69.660461};
+var ddLatLng_006 = {lat: 44.562982, lng: -69.660462};
+var ddLatLng_007 = {lat: 44.563372, lng: -69.660422};
+var ddLatLng_008 = {lat: 44.564047, lng: -69.660254};
+var ddLatLng_009 = {lat: 44.564460, lng: -69.660065};
+var ddLatLng_010 = {lat: 44.565225, lng: -69.659534};
+var ddLatLng_011 = {lat: 44.564955, lng: -69.659012};
+var ddLatLng_012 = {lat: 44.564701, lng: -69.658627};
+var ddLatLng_013 = {lat: 44.564518, lng: -69.658531};
+
+//DG ROUTE IS NOT CERTIAN, MAY BE CHANGED IF ROUTE DIFFERES FROM ASSUMEND ROUTE
+var dgLatLng_001 = {lat: 44.562064, lng: -69.659634};
+var dgLatLng_002 = {lat: 44.561983, lng: -69.659704};
+var dgLatLng_003 = {lat: 44.561908, lng: -69.660352};
+var dgLatLng_004 = {lat: 44.561336, lng: -69.660115};
+var dgLatLng_005 = {lat: 44.557957, lng: -69.658622};
+var dgLatLng_006 = {lat: 44.557434, lng: -69.658141};
+var dgLatLng_007 = {lat: 44.557197, lng: -69.657624};
+var dgLatLng_008 = {lat: 44.556345, lng: -69.654988};
+			
+var shuttleCoords = [
+	mainStLatLng,
+	mgLatLng_001,
+	mgLatLng_002,
+	gilmanStLatLng,
+	gdLatLng_001,
+	gdLatLng_002,
+	gdLatLng_003,
+	gdLatLng_004,
+	gdLatLng_005,
+	gdLatLng_006,
+	gdLatLng_007,
+	gdLatLng_008,
+	diamondLatLng,            
+	ddLatLng_001,
+	ddLatLng_002,
+	ddLatLng_003,
+	ddLatLng_004,
+	ddLatLng_005,
+	ddLatLng_006,
+	ddLatLng_007,
+	ddLatLng_008,
+	ddLatLng_009,
+	ddLatLng_010,
+	ddLatLng_011,
+	ddLatLng_012,
+	ddLatLng_013,
+	davisLatLng,
+	ddLatLng_013,
+	ddLatLng_012,
+	ddLatLng_011,
+	ddLatLng_010,
+	ddLatLng_009,
+	ddLatLng_008,
+	ddLatLng_007,
+	ddLatLng_006,
+	ddLatLng_005,
+	dgLatLng_003,
+	dgLatLng_004,
+	dgLatLng_005,
+	dgLatLng_006,
+	dgLatLng_007,
+	dgLatLng_008,
+	gilmanStLatLng,
+	mgLatLng_002,
+	mgLatLng_001,
+	mainStLatLng
+];
+
+
+
+
+
+
