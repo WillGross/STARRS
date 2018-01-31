af46f46b8743b5c44c7529e258cfa2cd0e4f9cb7
Zilin Chen <zichen@imac-025358.colby.edu>
2018-01-31 11:16:03 -0500

    sdfs

--- Source code/JitneyDriverPage.php
+++ Source code/JitneyDriverPage.php
@@ -46,7 +46,7 @@ try {
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     </script>
-    <script src="JitneyDriverPage.js"></script>
+    <script src="LogLocation.js"></script>
     <script src="JitneyUserPage.js"></script>
     <script src="JitneyGoogleMaps.js"></script>
     <link rel="stylesheet"
@@ -221,11 +221,59 @@ try {
 
     <div id="scheduleRequestPage">
         <div class="sectionTitle">
-            <h2>Today's schedule</h2>
+            <h2><?= date('D')?>'s schedule</h2>
             <table id="dailySchedule">
+            	<tr>
+            		<th>Time</th>
+            		<th>Schedule</th>
+            	</tr>
+            	
+	
                 <?php
-                // Find a way to represent the schedule, and make a table here
-                // to display the shift today for the user.
+                // Find a way to represent the schedule, and make a table here.
+                date_default_timezone_set('US/Easter');
+                $my_date = date('D');
+                // echo $my_date;
+                $arr = array(
+                	0 => "Sun",
+                	1 => "Mon",
+                	2 => "Tue",
+                	3 => "Wed",
+                	4 => "Thu",
+                	5 => "Fri",
+                	6 => "Sat"
+                );
+                // echo $arr[$my_date];
+                $myfile = fopen("daily_schedule.txt", "r") or die("Unable to open file!");
+                $week = array();
+                $int = 0;
+                while(! feof($myfile)){
+                	$week[$arr[$int]]= fgets($myfile);
+                	$int ++;
+                }
+                fclose($myfile);  
+               	//echo $week[$my_date];
+                $daily = explode(" ",$week[$my_date]);
+                $time = 1;
+                $class = "none";
+                foreach ($daily as $one){
+                	$one = str_replace(' ','',$one);
+                	$time = ($time + 1) % 12;
+                	if ($one !== "None"){
+                	
+                		$class = "driver_shift";
+                	} else {
+                		$class = "none";
+                		$one = '';
+                    }
+                ?>
+                	<tr>
+                		<td><?=$time?>:00</td>
+                		<td class="<?=$class?>" ><?=$one?></td>
+                	</tr>
+                <?php	
+                }
+                
                 ?>
             </table>
         </div>
@@ -235,15 +283,15 @@ try {
 
 <!-- Useful links provided to the user below the map. -->
 <div id="Links">
-    <p>
-        <a href="https://www.colby.edu/securitydept/colby-transportation-services/">
-            Colby Transportation Services (security office webpage)</a> <br>
-        <a href="http://www.colby.edu/securitydept/wp-content/uploads/sites/151/2017/09/Downtown-Shuttle-Schedule-and-Map-Sept2017.pdf">
-            Colby Shuttle Schedule and Map</a> <br>
-        <a href="JitneySchedule.php">
-            Jitney driver shifts</a><br>
+    <ul>
+        <li><a href="https://www.colby.edu/securitydept/colby-transportation-services/">
+            Colby Transportation Services (security office webpage)</a> </li>
+        <li><a href="http://www.colby.edu/securitydept/wp-content/uploads/sites/151/2017/09/Downtown-Shuttle-Schedule-and-Map-Sept2017.pdf">
+            Colby Shuttle Schedule and Map</a> </li>
+        <li><a href="JitneySchedule.php">
+            Jitney driver shifts</a></li>
         *The shuttle only runs from Monday to Friday during school sessions.
-    </p>
+    </ul>
 </div>
 
 <!--bottom of the page-->
