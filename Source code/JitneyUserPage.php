a99581a5dd4f518f1cf7a07fc69abfd67919dc66
Will Gross <wlgross@colby.edu>
2018-01-31 12:14:19 -0500

    added refrences to pulLocation.js

--- Source code/JitneyUserPage.php
+++ Source code/JitneyUserPage.php
@@ -45,6 +45,7 @@ try {
     </script>
 <!--    <script type="text/javascript" src="GoogleMapsVariables.js"></script>-->
     <script src="JitneyUserPage.js"></script>
+    <script src="pulLocation.js"></script>
     <script src="JitneyGoogleMaps.js"></script>
     <link rel="stylesheet"
           href="index.css<?php echo "?".time(); //To avoid server from caching CSS ?>"
