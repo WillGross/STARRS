1ffa618942f98d7eec09e603e24b442582235f24
Zhuofan Zhang <zhzhang@imac-025271.colby.edu>
2018-01-31 20:31:27 -0500

    Fixed JitneyGoogleMaps.js problem

--- Source code/JitneyGoogleMaps.js
+++ Source code/JitneyGoogleMaps.js
@@ -49,20 +49,27 @@ function newJitneyLocation() {
 var lat=44.556;
 var lon=-69.646;
 
-var jitneyLocation = new google.maps.LatLng(lat, lon);
+function initMap() {
+
+    var jitneyLocation = new google.maps.LatLng(lat, lon);
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: userLocation,
+        zoom: 14
+    });
 
 // Add a marker with the taxi-stand.svg as the icon.
 // Source: http://map-icons.com/
-var marker = new google.maps.Marker({
-    position: jitneyLocation,
-    map: map,
-    icon: 'taxi-stand.svg'
-});
+    var marker = new google.maps.Marker({
+        position: jitneyLocation,
+        map: map,
+        icon: 'taxi-stand.svg'
+    });
 
-newJitneyLocation();
-setInterval(newJitneyLocation, 5000);
+    newJitneyLocation();
+    setInterval(newJitneyLocation, 5000);
 
-function newJitneyLocation() {
-    var newLocation = new google.maps.LatLng(lat, lon);
-    marker.setPosition(newLocation);
+    function newJitneyLocation() {
+        var newLocation = new google.maps.LatLng(lat, lon);
+        marker.setPosition(newLocation);
+    }
 }
