ff9fb0439f2e6631efd5cda01931237a45505a3e
rmsellar <32618844+rmsellar@users.noreply.github.com>
2018-04-22 20:54:15 +0000

    Update jitney-google-maps-plugin.php
    Fixed marker and got marker to update every five seconds.

--- Unfinished_Template_Pluggins/jitney-google-maps-plugin.php
+++ Unfinished_Template_Pluggins/jitney-google-maps-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: STARRS Map
  * Description: A plugin displays a map tracking the movement of the jitney and shuttle
- * Version: 1.0
+ * Version: 1.5
  * Author: Ryan Sellar
  */
 
@@ -23,32 +23,22 @@ function jitney_map( $args ) {
 
 	<script type='text/javascript'>
 	var map;
+	var marker;
 	var jitneyLocation = { lat: <?php echo $args['lat'] ?>, lng: <?php echo $args['lng'] ?> }
 	function initMap() {
 	  map = new google.maps.Map(document.getElementById('map'), {
 	    center: jitneyLocation,
 	    zoom: <?php echo $args['zoom'] ?>
 	  });
-	}
-	var marker = new google.maps.Marker({
+	
+	  marker = new google.maps.Marker({
         position: jitneyLocation,
         map: map
     });
-	</script>
-
-	<?php
-	$output = ob_get_clean();
-	return $output;
-}
-
-//activates the google map api when wordpress loads header
-add_action( 'wp_head', 'mgms_enqueue_assets' );
-function mgms_enqueue_assets() {
-	wp_enqueue_script( 
-	  'google-maps', 
-	  '//maps.googleapis.com/maps/api/js?key=AIzaSyD7EpcuhwegwyYstrosisrWSMCu8vqiKCE&callback=initMap', 
-	  array(), 
-	  '1.0', 
-	  true 
-	);
-}
+	}
+	
+	var cycle = setInterval(updateMarker, 5000);
+	//placeholder for actual latitude and longitude from php script
+	var latitude = <?php echo $args['lat'] ?>;
+	var longitude = <?php echo $args['lng'] ?>;
+	function updateMarker(){
