e65601e7f8226deb381f7b02e0202324181d41e0
Paige Hanssen <pvhanssen@gmail.com>
2018-05-02 22:31:18 -0400

    Finished request form
    Request form properly submits to database, changes fields for driver and dispatcher based on true/false in shortcode, resets form when request has been made.

--- Finished_Pluggins/jitney-request-form.php
+++ Finished_Pluggins/jitney-request-form.php
@@ -7,41 +7,44 @@ Author: Paige Hanssen
 */
 
 // function that sets up the form with html
-function jitney_request_form($pickup, $destination, $num_people, $comments, $dispatcher) {
+function jitney_request_form($pickup, $destination, $num_people, $comments, $dispatcher, $driverRequest) {
     // extra field created in the form if a dispatcher is logged in
     if ($dispatcher == "True") {
         $myColbyUser = 
             '<div>
                 <label for="username">Caller\'s myColby username:<strong>*</strong></label>
-                <input type="textarea" name="username" value="' . ( isset( $_POST['username'] ) ? $dispatcher : null ) . '">
+                <input type="textarea" name="username" value="">
             </div>';
     }
 
-    if($driver = "True") {
+    // if the driverRequest is making the request, then auto-fill pickup spot to Spa
+    if($driverRequest == "True") {
         $driverLocation = "Spa";
+    } else {
+        $driverLocation = $pickup;
     }
 
-    // building the actual form - fields for pickup, destination, number of people and comments
+    // building the actual form - fields for pickup, destination, number of people, username and comments
     $pluginContent =
         '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
         <div>
             <label for="pickup">Enter pickup location <strong>*</strong></label>
-            <input type="textarea" name="pickup" value="' . (isset($driverLocation) ? $driverLocation : (isset( $_POST['pickup'] ) ? $pickup : null )) . '">
+            <input type="textarea" name="pickup" value="' . $driverLocation . '">
         </div>
         
         <div>
             <label for="destination">Enter dropoff location <strong>*</strong></label>
-            <input type="textarea" name="destination" value="' . ( isset( $_POST['destination'] ) ? $destination : null ) . '">
+            <input type="textarea" name="destination" value="">
         </div>
         
         <div>
             <label for="num_people">How many people are traveling? <strong>*</strong></label>
-            <input type="select" name="num_people" value="' . ( isset( $_POST['num_people']) ? $num_people : null ) . '">
+            <input type="select" name="num_people" value="">
         </div>
         
         <div>
             <label for="comments">Additional comments:</label>
-            <input type="textarea" name="comments" value="' . ( isset( $_POST['comments']) ? $comments : null ) . '">
+            <input type="textarea" name="comments" value="">
         </div>'
         . ( isset($myColbyUser) ? $myColbyUser : '' ) .
         '<input type="submit" name="submit" value="Request"/>
@@ -57,7 +60,7 @@ function request_validation($pickup, $destination, $num_people, $comments, $user
     $reg_errors = new WP_Error;
 
     //display error is required field is not filled out
-    if ( empty( $pickup ) || empty( $destination ) ) {
+    if ( empty( $pickup ) || empty( $destination || empty($num_people) || empty($username)) ) {
         $reg_errors->add('field', 'Required form field is missing');
     }
 
@@ -73,46 +76,50 @@ function request_validation($pickup, $destination, $num_people, $comments, $user
 
 // submit data to ride_requests table
 function complete_request() {
-    global $wpdb, $reg_errors, $pickup, $destination, $num_people, $comments;
+    global $wpdb, $reg_errors, $pickup, $destination, $num_people, $comments, $username;
+    request_validation($pickup, $destination, $num_people, $comments, $username);
 
-    //access device time of request -> timeOfCall, shift_ID, pickupTime, dropoffTime
+    // array of values maps to database insert fields
     $data = array(
-        // 1,1,'wlgross',CURRENT_TIMESTAMP ,CURRENT_TIMESTAMP , CURRENT_TIMESTAMP ,'here','there','pending request 1 from wlgross', 2, 'pending'
-        'shift_ID' => 1,
-        'userName' => 'phans',
+        'shift_ID' => 1,                            // get shift id from who is currently driving
+        'userName' => $username,
         'timeOfCall' => current_time('mysql', 1),
         'pickupTime' => current_time('mysql', 1),   // - originally set to null?
         'dropoffTime' => current_time('mysql', 1),  // - originally set to null?
         'pickupLocation' => $pickup,
         'dropoffLocation' => $destination,
+        'comments' => $comments,
         'numPeople' => $num_people,
-        'rideStatus' => 'pending'            // if a driver fills out form, it is 'En route' and not pending
+        'rideStatus' => 'pending'               // if a driverRequest fills out form, it is 'En route' and not pending
     );
-    $wpdb->insert( 'ride_requests', $data );    //insert function in WP takes in table and array of data
-    echo "Request completed.";                  //send user a message when request is done
+    $wpdb->insert( 'ride_requests', $data );    // insert function in WP takes in table and array of data
+    $pickup = '';                               // reset the pickup field to empty for non-driver forms
+    echo "Request completed.";                  // send user a message when request is done
 }
 
 function custom_request_function($atts) {
+    // map attributes to values
     $a = shortcode_atts( array(
         'dispatcher' => null,
-        'driver' => null
+        'driver' => "False"      // for some reason, driver is not being read as part of attributes if originally set to null
     ), $atts );
     $dispatcher = $a['dispatcher'];
-    $driver = $a['driver'];
+    $driverRequest = $a['driver'];
 
     //when submit button is clicked, call request complete function
     if ( isset($_POST['submit'] ) ) {
-        if($dispatcher) {
-            $username = ( $_POST['username'] );
-        } else {
-            $username = null;
-        }
     
         global $pickup, $destination, $num_people, $comments, $username;
-        $pickup   =   ( $_POST['pickup'] );
-        $destination   =  ( $_POST['destination'] );
-        $num_people      =   ( $_POST['num_people'] );
-        $comments    =  ( $_POST['comments'] );
+        $pickup         = ( $_POST['pickup'] );
+        $destination    = ( $_POST['destination'] );
+        $num_people     = ( $_POST['num_people'] );
+        $comments       = ( $_POST['comments'] );
+        if($dispatcher) {
+            $username   = ( $_POST['username'] );
+        } else {
+            $username   = 'user';   // get username from cookies
+        }
+
 
         // call complete_request function
         complete_request(
@@ -131,7 +138,7 @@ function custom_request_function($atts) {
         $num_people,
         $comments,
         $dispatcher,
-        $driver
+        $driverRequest
     );
 }
 
