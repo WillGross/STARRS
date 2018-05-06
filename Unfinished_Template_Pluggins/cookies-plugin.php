9fdcb80fdd1fd0d6f36988f47c598188389c695a
rmsellar <32618844+rmsellar@users.noreply.github.com>
2018-05-06 06:34:30 +0000

    Update cookies-plugin.php

--- Unfinished_Template_Pluggins/cookies-plugin.php
+++ Unfinished_Template_Pluggins/cookies-plugin.php
@@ -1,1 +1,20 @@
+<?php
+/**
+ * Plugin Name: STARRS Cookies
+ * Description: Plugin sets up cookies for use in other plugins
+ * Version: 1.0
+ * Author: Ryan Sellar
+ */
 
+add_action( 'init', 'set-up-cookies' );
+
+function set_up_cookies() {
+  var authentication = //however you get the users authentication
+  var shift-id = //however you get the current shift-id
+  setcookie( $v_username, authentication, DAY_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN );
+  setcookie( 'current-shift', shift-id, DAY_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN );
+}
+
+//to access a cookie use $_COOKIE[<cookie name>]
+//for example: $_COOKIE[$v_username] gets the cookie assigned to the current username
+// $_COOKIE['current-shift'] gets the cookie assigned to the current shift
