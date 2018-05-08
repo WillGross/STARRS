9158970b790afd65b222c161f6461deaf8432282
rmsellar <32618844+rmsellar@users.noreply.github.com>
2018-05-08 04:25:53 +0000

    Reverted to old version

--- Unfinished_Template_Pluggins/jitney-google-maps-plugin.php
+++ Unfinished_Template_Pluggins/jitney-google-maps-plugin.php
@@ -10,9 +10,6 @@
 //[map lat='<latitude>' lng='longitude' zoom='<zoom>' height='<height>']
 add_shortcode( 'map', 'jitney_map' );
 function jitney_map( $args ) {
-	global $wpdb;
-	$vehicleLocations = $wpdb -> get_results('SELECT vehicle_ID, latitude, longitude from location');
-	$vehicleStatus = $wpdb -> get_results('SELECT id, isDriving from vehicles');
 	$args = shortcode_atts( array(
 		'lat'    => '44.556',
 		'lng'    => '-69.646',
@@ -26,40 +23,32 @@ function jitney_map( $args ) {
 
 	<script type='text/javascript'>
 	var map;
-	var markers = new Array();
-	var ids = new Array(<?php echo $vehicleStatus['id'] ?>);
-	var center = { lat: <?php echo $args['lat'] ?>, lng: <?php echo $args['lng'] ?> }
+	var marker;
+	var jitneyLocation = { lat: <?php echo $args['lat'] ?>, lng: <?php echo $args['lng'] ?> }
 	function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: center,
-			zoom: <?php echo $args['zoom'] ?>
-		});
-	}
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    center: jitneyLocation,
+	    zoom: <?php echo $args['zoom'] ?>
+	  });
 	
-	function initMarkers(){
-		var i = 0;
-		var drivingArray = new Array(<?php echo $vehicleStatus['isDriving'] ?>);
-		var vehicleIDArray = new Array(<?php echo $vehicleLocations['vehicle_ID'] ?>);
-		for (i = 0; i < ids.length; i++):
-			if drivingArray[i] == 'yes':
-				marker = new google.maps.Marker({
-					position: jitneyLocation,
-					map: map,
-					label: String(vehicleIDArray[i])
-    				});
-				markers.push(marker);
+	  marker = new google.maps.Marker({
+        position: jitneyLocation,
+        map: map
+    });
 	}
 	
 	var cycle = setInterval(updateMarker, 5000);
-	var latitude = <?php echo $vehicleLocations['latitude'] ?>;
-	var longitude = <?php echo $vehicleLocations['longitude'] ?>;
-	function updateMarkers(){
-		var i;
-		for (i = 0; i < markers.length; i++):
-			marker = markers[i];
-			marker.setMap(null);
-			marker.setPosition({ lat: latitude[i], lng: longitude[i] });
-			marker.setMap(map);
+	//placeholder for actual latitude and longitude from php script
+	var latitude = <?php echo $args['lat'] ?>;
+	var longitude = <?php echo $args['lng'] ?>;
+	function updateMarker(){
+		marker.setMap(null);
+		//below is just for testing updating
+		latitude = latitude + 0.001;
+		longitude = longitude + 0.001;
+		//set position should use latitude and longitude from logLocation
+		marker.setPosition({ lat: latitude, lng: longitude });
+		marker.setMap(map);
 	}
 	</script>
 
@@ -69,8 +58,8 @@ function jitney_map( $args ) {
 }
 
 //activates the google map api when wordpress loads header
-add_action( 'wp_head', 'jitney_enqueue_assets' );
-function jitney_enqueue_assets() {
+add_action( 'wp_head', 'mgms_enqueue_assets' );
+function mgms_enqueue_assets() {
 	wp_enqueue_script( 
 	  'google-maps', 
 	  '//maps.googleapis.com/maps/api/js?key=AIzaSyD7EpcuhwegwyYstrosisrWSMCu8vqiKCE&callback=initMap', 
